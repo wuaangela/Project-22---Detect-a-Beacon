fd0ea9c033766196435d90bbe3ba01e555d02be2
A W <waq.mmhy@live.ca>
2019-07-30 14:49:06 -0400

    Initial Commit

---
+++ Project 22 - Detect a Beacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 22 - Detect a Beacon
+//
+//  Created by Angela Wu  on 2019-07-30.
+//  Copyright © 2019 Angela Wu . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
